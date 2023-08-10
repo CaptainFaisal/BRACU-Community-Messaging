33321a372ab4bbc58839b1dc2258b9fdae20f43d
udoysaha103 <udoysaha103@gmail.com>
2023-08-11 02:03:29 +0600

    Initital commit. Just setting up the frontend somehow

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import ListGroup from "./Components/ListGroup"
+
+function App() {
+  return (
+    <ListGroup />
+  );
+}
+
+export default App;
---
+++ frontend/src/Components/ListGroup.tsx
@@ -0,0 +1,10 @@
+
+function ListGroup() {
+    return (
+        <>
+            { true ? <h1>Hi</h1> : <h1>Hello</h1>}
+        </>
+    );
+}
+
+export default ListGroup;
