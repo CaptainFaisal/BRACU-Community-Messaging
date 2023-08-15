47594b53bf896940db47bdc47b5479af7a29a81e
captainfaisal <faisalhawlader04@gmail.com>
2023-08-15 14:56:40 +0600

    added login.tsx functionality

--- frontend/src/Components/Login/Login.tsx
+++ frontend/src/Components/Login/Login.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import MiddleBox from "../MiddleBox/MiddleBox";
+import axios from "axios";
 import "./Login.css";
 
 function Login() {
@@ -39,7 +40,7 @@ function Login() {
     background: "rgba(42, 95, 172, 0.60)",
     marginTop: "2vh",
     boxShadow: "0px 4px 10px 0px rgba(0, 0, 0, 0.25)",
-  }
+  };
 
   const signup_text = {
     color: "#000",
@@ -50,18 +51,28 @@ function Login() {
     lineHeight: "normal",
     margin: "auto",
     marginTop: "10vh",
-  }
+  };
 
   const navigate = useNavigate();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [erromsg, setErrormsg] = useState("");
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     console.log("Email: " + email + " Password: " + password);
-
+    axios
+      .post("http://localhost:3000/user", { email: email, password: password })
+      .then((res) => {
+        console.log(res.data)
+        if(res.data.length==1){
+          navigate("/home");
+        }else{
+          setErrormsg("Invalid credentials");
+        }
+      })
+      .catch((err) => console.log(err));
     // if email and password are correct, navigate to newsfeed with correct user profile (need to implement)
-    navigate('/home');
+
   };
 
   return (
@@ -69,21 +80,59 @@ function Login() {
       <form className="login_form" onSubmit={handleSubmit}>
         <p className="text-center section_text">Login</p>
         {/* User name or email field */}
-        <input type="email" className="form-control mb-3" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Username or Email" style={{...input_box, marginTop: "5vh"}} onChange={(event) => setEmail(event.target.value)} required/>
+        <input
+          type="email"
+          className="form-control mb-3"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+          placeholder="Username or Email"
+          style={{ ...input_box, marginTop: "5vh" }}
+          onChange={(event) => setEmail(event.target.value)}
+          required
+        />
         {/* Password filed */}
-        <input type="password" className="form-control mb-3" id="exampleInputPassword1" placeholder="Password" style={{...input_box, marginTop: "4vh"}} onChange={(event) => setPassword(event.target.value)} required/>
+        <input
+          type="password"
+          className="form-control mb-3"
+          id="exampleInputPassword1"
+          placeholder="Password"
+          style={{ ...input_box, marginTop: "4vh" }}
+          onChange={(event) => setPassword(event.target.value)}
+          required
+        />
         {/* Forgot password link */}
-        <a href="#" className="forgot_password">Forgot Password?</a>
-        <button type="submit" className="btn btn-primary" style={{...button_style}} >Let's go</button>
+        <p style={{color: "red", margin: "auto"}}>{erromsg}</p>
+        <a href="#" className="forgot_password">
+          Forgot Password?
+        </a>
+        <button
+          type="submit"
+          className="btn btn-primary"
+          style={{ ...button_style }}
+        >
+          Let's go
+        </button>
       </form>
 
-      <form className="login_form" onSubmit={() => navigate('/signup')}>
+      <form className="login_form" onSubmit={() => navigate("/signup")}>
         {/* Signup section */}
-        <p className="text-center" style={{...signup_text}}>Do not have an account?</p>
-        <button type="submit" className="btn btn-primary" style={{...button_style, color: "rgba(42, 95, 172)", background: "rgba(255, 255, 255, 0.60)",}}>Sign Up</button>
+        <p className="text-center" style={{ ...signup_text }}>
+          Do not have an account?
+        </p>
+        <button
+          type="submit"
+          className="btn btn-primary"
+          style={{
+            ...button_style,
+            color: "rgba(42, 95, 172)",
+            background: "rgba(255, 255, 255, 0.60)",
+          }}
+        >
+          Sign Up
+        </button>
       </form>
     </MiddleBox>
-  )
+  );
 }
 
 export default Login;
--- frontend/src/Components/Navbar/Navbar.tsx
+++ frontend/src/Components/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import "./navbar.css";
+import "./Navbar.css";
 
 function Navbar() {
   const SearchBarStyle = {
