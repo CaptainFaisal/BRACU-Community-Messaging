6bf60b2615fdeb4fbf586196f05fe7118b7a60df
captainfaisal <faisalhawlader04@gmail.com>
2023-08-17 01:15:19 +0600

    hello

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -10,6 +10,23 @@ router.post('/', (req, res)=> {
     else res.send(result);
     })
 })
+router.post('/new', (req, res) => {
+    const data = req.body;
+    db.query(`SELECT * FROM users WHERE email = "${data.email}"`, (err, result) => {
+        if(err){
+            console.log(err);
+        } else {
+            if(result.length > 0) {
+                res.send({msg: "Email already exists!"});
+            } else{
+                db.query(`INSERT INTO users (firstname, lastname, dob, password, gender, email, phone, time_stamp) VALUES ("${data.firstName}", "${data.lastName}", "${data.dob}", "${data.password}", "${data.gender === "F"?0:data.gender==="M"?1:2}", "${data.email}", "${data.phone}", CURRENT_TIMESTAMP())`, er => {
+                    if(er) console.log(er)
+                    else res.send({msg: "success"});
+                })
+            }
+        }
+    })
+})
 
 router.get('/:userId', (req, res) => {
     const userId = req.params.userId;
--- backend/server.js
+++ backend/server.js
@@ -17,22 +17,7 @@ app.use('/testDB', (req, res) => {
         }
     });
 })
-// app.get('/insertdummyuser', (req, res) => {
-//     // const query = "INSERT INTO user VALUES (1, 'fname', 'lname', '1999-03-15', 'right answer of the mcq', 'md5hashing', 'dp_link', 'cp_link', '1', 'example@xyz.com', '1234567890', 1, CURRENT_TIMESTAMP())";
-
-//     for(let i=1; i<=100; i++) {
-//         db.query(`INSERT INTO user VALUES (${i}, 'fname ${i}', 'lname ${i}', '1999-03-15', 'right answer of the mcq', 'md5hashing', 'dp_link', 'cp_link', '1', 'example@xyz.com', '1234567890', 1, CURRENT_TIMESTAMP())`
-//         , (err, result) => {
-//             if(err) {
-//                 console.log(err);
-//             } else {
-//                 console.log('Inserted');
-//             }
-//         });
-//     }
-// });
-
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log('Server is running on port ' + PORT);
 });
